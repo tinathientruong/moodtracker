3ece41610dbef7b55416aa4bfbe6e134e36143e8
Tolu Adegbite <tolu@Tolus-MacBook-Pro.local>
2019-02-04 20:15:21 -0500

    styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
           <p className="moodioIntro">
           What’s the key to a happy, fulfilled life? <br/>
           Mindfulness. <br/>
-          Mind your everyday mood, see the long-term trends <br/>
+          Log your everyday mood, see the long-term trends <br/>
           and understand what makes you happy and what doesn’t.
           </p>
          <img className="happyLife" src={'https://uploads-ssl.webflow.com/5bff8886c3964a992e90d465/5bff98f85ea7faf86504605e_scenes.gif'}/>
