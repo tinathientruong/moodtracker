e9c09bea4683a93b5a4f4fd2f7dade8cdc0f82bf
Tolu Adegbite <tolu@Tolus-MacBook-Pro.local>
2019-02-04 19:58:16 -0500

    primary homepage styling

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,7 +6,7 @@ class Nav extends Component {
   render() {
     return (
       <div className="navWrapper">
-        <NavLink className="navItem" exact activeClassName="selected" to="/"> Go To Home Page </NavLink>
+        <NavLink className="navItem" exact activeClassName="selected" to="/"> Home </NavLink>
         <NavLink className="navItem" to="/calendar" activeClassName="selected" > Calendar </NavLink> 
         <NavLink className="navItem" to="/blog" activeClassName="selected"> Entries</NavLink>
       </div>
